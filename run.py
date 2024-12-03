bf311adafd2ca6f652aaa1547658855c495c7594
DinhLuong04 <166143726+DinhLuong04@users.noreply.github.com>
2024-12-03 20:03:19 +0700

    update

---
+++ run.py
@@ -0,0 +1,7 @@
+from src.app import EightPuzzle
+
+if __name__ == '__main__':
+    app = EightPuzzle()
+    app.mainloop()
+
+ 
---
+++ src/config.py
@@ -0,0 +1,72 @@
+# colors
+DARK_BLUE = '#0d1b2a'
+LIGHT_BLUE = '#1b263b'
+AQUA = '#415a77'
+ORANGE = '#e63946'
+LIGHT_YELLOW = '#ffba08'
+SOFT_WHITE = '#f1faee'
+
+# widget properties
+BASIC_FRAME_PROPERTIES = {
+    'background': DARK_BLUE
+}
+
+HEADING_LABEL_PROPERTIES = {
+    'font': ('Poppins', 48, 'bold'),
+    'background': DARK_BLUE,
+    'foreground': SOFT_WHITE
+}
+
+SUBHEADING_LABEL_PROPERTIES = {
+    'font': ('Roboto', 16, 'italic'),
+    'background': DARK_BLUE,
+    'foreground': LIGHT_YELLOW
+}
+
+TEXT_LABEL_PROPERTIES = {
+    'font': ('Poppins', 20, 'normal'),
+    'background': DARK_BLUE,
+    'foreground': AQUA
+}
+
+PRIMARY_BUTTON_PROPERTIES = {
+    'font': ('Roboto', 18, 'bold'),
+    'background': AQUA,
+    'foreground': SOFT_WHITE,
+    'activebackground': LIGHT_BLUE,
+    'activeforeground': SOFT_WHITE,
+    'width': 10,
+    'border': 0,
+    'relief': 'flat'
+}
+
+SECONDARY_BUTTON_PROPERTIES = {
+    'font': ('Roboto', 18, 'bold'),
+    'background': ORANGE,
+    'foreground': SOFT_WHITE,
+    'activebackground': LIGHT_BLUE,
+    'activeforeground': SOFT_WHITE,
+    'width': 10,
+    'border': 0,
+    'relief': 'flat'
+}
+
+TERTIARY_BUTTON_PROPERTIES = {
+    'font': ('Roboto', 18, 'bold'),
+    'background': LIGHT_YELLOW,
+    'foreground': DARK_BLUE,
+    'activebackground': LIGHT_BLUE,
+    'activeforeground': SOFT_WHITE,
+    'width': 10,
+    'border': 0,
+    'relief': 'flat'
+}
+
+TILE_BUTTON_PROPERTIES = {
+    'background': DARK_BLUE,
+    'foreground': AQUA,
+    'activebackground': DARK_BLUE,
+    'activeforeground': AQUA,
+    'disabledforeground': LIGHT_BLUE,
+    'border': 0
+}
